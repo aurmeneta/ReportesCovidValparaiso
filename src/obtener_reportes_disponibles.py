027a8f1e519c689111aaf7458b6aad3209525920
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2021-08-30 16:27:29 -0400

    Error catching y retries al obtener reportes disp.

--- src/obtener_reportes_disponibles.py
+++ src/obtener_reportes_disponibles.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime
-
 from util import string_to_datetime, Reporte
 
 START_DATE = datetime.date(2021, 2, 1)
@@ -9,28 +8,37 @@ URL_BASE = "https://seremi5.redsalud.gob.cl"
 URL_REPORTES = "https://seremi5.redsalud.gob.cl/reporte-regional/"
 
 
-def obtener_reportes_disponibles():
-    # Descargar página del SEREMI.
-    html = requests.get(URL_REPORTES)
-    # Cargar página a bs.
-    soup = BeautifulSoup(html.content, "html.parser")
-    # Buscar tag section que es parent de los links a los reportes.
-    section = soup.find("section", class_="body")
-    # Buscar todos los tags a
-    links = section.find_all("a")
+def obtener_reportes_disponibles(retry=0):
+    print("Obteniendo reportes disponibles. Intento", retry)
+    try:
+        # Descargar página del SEREMI.
+        html = requests.get(URL_REPORTES)
+        # Cargar página a bs.
+        soup = BeautifulSoup(html.content, "html.parser")
+        # Buscar tag section que es parent de los links a los reportes.
+        section = soup.find("section", class_="body")
+        # Buscar todos los tags a
+        links = section.find_all("a")
 
-    # Iniciar lista para guardar los reportes disponibles.
-    reportes = []
+        # Iniciar lista para guardar los reportes disponibles.
+        reportes = []
 
-    for a in links:
-        # Obtener fecha del reporte
-        date = string_to_datetime(a.contents[0])
-        if date >= START_DATE:
-            link = URL_BASE + a["href"]
-            reportes.append(Reporte(date, link))
-            print(Reporte(date, link))
+        for a in links:
+            # Obtener fecha del reporte
+            date = string_to_datetime(a.contents[0])
+            if date >= START_DATE:
+                link = URL_BASE + a["href"]
+                reportes.append(Reporte(date, link))
+                print(Reporte(date, link))
 
-    return reportes
+        return reportes
+    except Exception as e:
+        print("Error al obtener la lista de informes")
+        print(e)
+        if retry < 10:
+            return obtener_reportes_disponibles(retry + 1)
+        else:
+            return []
 
 
 if __name__ == "__main__":
